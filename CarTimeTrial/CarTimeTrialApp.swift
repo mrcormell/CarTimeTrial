ec16202fb17e631f323f24a1c404841aec0904b6
David Cormell <d.cormell@etoncollege.org.uk>
2024-02-01 14:22:15 +0000

    Initial Commit

---
+++ CarTimeTrial/CarTimeTrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarTimeTrialApp.swift
+//  CarTimeTrial
+//
+//  Created by Cormell, David - DPC on 01/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CarTimeTrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
