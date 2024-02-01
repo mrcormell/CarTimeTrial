4daaf7ba0589b60a70c8dbd7ee74405492df7b90
David Cormell <d.cormell@etoncollege.org.uk>
2024-02-01 14:27:17 +0000

    Tests for displayStats()

--- CarTimeTrialTests/CarTimeTrialTests.swift
+++ CarTimeTrialTests/CarTimeTrialTests.swift
@@ -10,27 +10,21 @@ import XCTest
 
 final class CarTimeTrialTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testCarGetStatsAsPerPropertyValues() {
+        //arrange
+        let car = Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5)
+        let expected = """
+        Make: Mazda
+        Model: MX-5
+        Top Speed: 125mph
+        Acceleration (0-60 in): 7.7s
+        Handling: 5
+        """
+        //arrange
+        let actual = car.displayStats()
+        //assert
+        XCTAssertEqual(actual, expected)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
